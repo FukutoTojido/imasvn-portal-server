0beefc2a0b751ddec8abd573a318922707de805e
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2025-03-04 12:33:51 +0700

    feat: added guard for WHEP endpoint

--- src/whep/index.ts
+++ src/whep/index.ts
@@ -1,11 +1,15 @@
 import { Elysia, t } from "elysia";
+import checkToken from "../middleware";
 
 const whep = new Elysia({ prefix: "/whep" }).post(
 	"/",
-	async ({ body: { sdp }, error }) => {
+	async ({ body: { sdp }, cookie: { refresh_token }, error }) => {
 		if (!process.env.STREAM_ENDPOINT)
 			return error(500, "Internal Server Error");
 		try {
+			if (!(await checkToken(refresh_token.value)))
+				return error(401, "Unauthorized");
+			
 			const res = await fetch(process.env.STREAM_ENDPOINT, {
 				method: "POST",
 				body: sdp,
@@ -25,9 +29,12 @@ const whep = new Elysia({ prefix: "/whep" }).post(
 			uid: t.Optional(t.String()),
 			sdp: t.String(),
 		}),
-        detail: {
-			tags: ["Live"]
-		}
+		cookie: t.Object({
+			refresh_token: t.Optional(t.String()),
+		}),
+		detail: {
+			tags: ["Live"],
+		},
 	},
 );
 
