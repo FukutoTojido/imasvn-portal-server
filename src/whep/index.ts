cac680dcf84462c150aab762696a60888d527bf8
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2025-09-17 15:48:05 +0700

    feat: switch to MediaMTX

--- src/whep/index.ts
+++ src/whep/index.ts
@@ -1,41 +1,104 @@
 import { Elysia, t } from "elysia";
 import checkToken from "../middleware";
 
-const whep = new Elysia({ prefix: "/whep" }).post(
-	"/",
-	async ({ body: { sdp }, cookie: { refresh_token }, error }) => {
-		if (!process.env.STREAM_ENDPOINT)
-			return error(500, "Internal Server Error");
-		try {
-			if (!(await checkToken(refresh_token.value)))
-				return error(401, "Unauthorized");
-			
-			const res = await fetch(process.env.STREAM_ENDPOINT, {
-				method: "POST",
-				body: sdp,
-				headers: {
-					"Content-type": "application/sdp",
-				},
-			});
+const whep = new Elysia({ prefix: "/whep" })
+	.options(
+		"/",
+		async ({ error, set, request }) => {
+			if (!process.env.STREAM_ENDPOINT)
+				return error(500, "Internal Server Error");
 
-			const data = await res.text();
-			return data;
-		} catch (e) {
-			return error(500, "Internal Server Error");
-		}
-	},
-	{
-		body: t.Object({
-			uid: t.Optional(t.String()),
-			sdp: t.String(),
-		}),
-		cookie: t.Object({
-			refresh_token: t.Optional(t.String()),
-		}),
-		detail: {
-			tags: ["Live"],
+			try {
+				const res = await fetch(process.env.STREAM_ENDPOINT, {
+					method: "OPTIONS",
+				});
+
+				set.status = 204;
+				const link = res.headers.get("Link")
+					? { Link: res.headers.get("Link") as string }
+					: undefined;
+				set.headers = {
+					...link,
+					"Access-Control-Allow-Headers":
+						"Authorization, Content-Type, If-Match",
+					"Access-Control-Expose-Headers": "Link",
+					"Access-Control-Allow-Origin": request.headers.get(
+						"origin",
+					) as string,
+					"Access-Control-Allow-Methods": "OPTIONS",
+					"Access-Control-Allow-Credentials": "true",
+				};
+			} catch (e) {
+				console.error(e);
+				return error(500, "Internal Server Error");
+			}
 		},
-	},
-);
+		{
+			detail: {
+				tags: ["Live"],
+			},
+		},
+	)
+	.post(
+		"/",
+		async ({ body, cookie: { refresh_token }, error, set, request }) => {
+			if (!process.env.STREAM_ENDPOINT)
+				return error(500, "Internal Server Error");
+			try {
+				if (!(await checkToken(refresh_token.value)))
+					return error(401, "Unauthorized");
+
+				const res = await fetch(process.env.STREAM_ENDPOINT, {
+					method: "POST",
+					body: body as string,
+					headers: {
+						"Content-type": "application/sdp",
+						credentials: "omit",
+					},
+				});
+
+				switch (res.status) {
+					case 201:
+						break;
+					case 404:
+						return error(404, "Stream Not Found");
+					case 400:
+						return res.json().then((e) => {
+							return error(400, e.error);
+						});
+					default:
+						return error(res.status, `bad status code ${res.status}`);
+				}
+
+				set.headers = {
+					location: res.headers.get("Location") as string,
+					"Access-Control-Allow-Headers":
+						"Authorization, Content-Type, If-Match",
+					"Access-Control-Expose-Headers": "ETag, ID, Accept-Patch, Link, Location",
+					"Access-Control-Allow-Origin": request.headers.get(
+						"origin",
+					) as string,
+					"Access-Control-Allow-Credentials": "true",
+				};
+
+				set.status = 201;
+
+				const data = await res.text();
+				return data;
+			} catch (e) {
+				console.error(e);
+				return error(500, "Internal Server Error");
+			}
+		},
+		{
+			body: t.String(),
+			cookie: t.Object({
+				refresh_token: t.Optional(t.String()),
+			}),
+			detail: {
+				tags: ["Live"],
+			},
+		},
+	);
 
 export default whep;
