fa70fded2ac5711a015de58230930d620fc39b6c
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2026-08-26 23:58:20 +0700

    feat: prod dev?

--- src/connection.ts
+++ src/connection.ts
@@ -6,7 +6,8 @@ const pool = mariadb.createPool({
 	user: process.env.DATABASE_USER,
 	password: process.env.DATABASE_PWD,
 	database: process.env.DATABASE_DB,
-	allowPublicKeyRetrieval: true
+	port: +(process.env.DATABASE_PORT ?? 0) || undefined,
+	allowPublicKeyRetrieval: true,
 });
 
 const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_PARAMS || ""}`;
